5dd63e3b50fdc22722d7d4bd3acf83f5a703f3f2
SOGOOD <78962359+DK5501@users.noreply.github.com>
2025-04-25 15:12:14 +0700

    Update settings.py

--- student_register/settings.py
+++ student_register/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-key'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['student-register-cgp6.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
